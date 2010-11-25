a291dcbe929830c1a528ae4b43222a7fcbe8e860
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2010-11-25 21:06:52 +0000

    HOMEBREW

--- trunk/mk.bat
+++ trunk/mk.bat
@@ -2,5 +2,6 @@ copy MKIJIRO.prx MKIJIRO\MKIJIRO.prx
 copy MKIJIRO_SC.prx MKIJIRO\MKIJIRO_SC.prx
 del *.elf
 del objects\*.o
+move MKIJIRO_POPS.prx g:\seplugins\MKIJIRO_POPS.prx
 move MKIJIRO_SC.prx g:\seplugins\mkultra.prx
 move MKIJIRO.prx g:\seplugins\mkultra_psplink.prx
