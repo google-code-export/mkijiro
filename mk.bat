801fefc3eb0a7af36efa67033b8c7d6490c6f9f4
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2010-11-26 17:16:05 +0000

    HOMEBREWGENをパス(hbpath)をダンプして読ますように変更、0x140だけ対応(M33/GEN) POPS版は通常UMDゲームでも一応うごくのでID処理共通化。 now support HOMEBREW cheat HACK(name offset 0x140only) POPSver,normal UMDgame/HB hybridmode

--- mk.bat
+++ mk.bat
@@ -1,6 +1,6 @@
 copy MKIJIRO.prx MKIJIRO\MKIJIRO.prx
 copy MKIJIRO_SC.prx MKIJIRO\MKIJIRO_SC.prx
-copy MKIJIRO_POPS.prx MKIJIRO_POPS.prx
+copy MKIJIRO_POPS.prx MKIJIRO\MKIJIRO_POPS.prx
 del *.elf
 del objects\*.o
 move MKIJIRO_POPS.prx g:\seplugins\MKIJIRO_POPS.prx
