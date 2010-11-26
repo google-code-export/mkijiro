52c30f969aa469880d7319515df220cf5c63c77f
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2010-11-26 15:52:42 +0000

    GENのみHB修正

--- mk.bat
+++ mk.bat
@@ -1,7 +1,8 @@
 copy MKIJIRO.prx MKIJIRO\MKIJIRO.prx
 copy MKIJIRO_SC.prx MKIJIRO\MKIJIRO_SC.prx
+copy MKIJIRO_POPS.prx MKIJIRO_POPS.prx
 del *.elf
 del objects\*.o
 move MKIJIRO_POPS.prx g:\seplugins\MKIJIRO_POPS.prx
-move MKIJIRO_SC.prx g:\seplugins\mkultra.prx
-move MKIJIRO.prx g:\seplugins\mkultra_psplink.prx
+move MKIJIRO_SC.prx g:\seplugins\MKIJIRO_SC.prx
+move MKIJIRO.prx g:\seplugins\MKIJIRO.prx
