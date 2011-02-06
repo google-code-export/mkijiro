8a7c1f85f32978c2ccac5f0dc49863667d33ba7a
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2011-02-06 16:09:37 +0000

    635PROのGID修正

--- trunk/NPR.bat
+++ trunk/NPR.bat
@@ -1,5 +1,9 @@
+cd SRC_nitePR_revJ
+make
+cd ..
 del SRC_nitePR_revJ\*.elf
 del SRC_nitePR_revJ\*.o
 copy SRC_nitePR_revJ\nitePR.prx SRC_nitePR_revJ\nitePR\nitePR.prx
 copy SRC_nitePR_revJ\nitePR.prx patches\nitePR.prx
+move SRC_nitePR_revJ\nitePR.prx g:\seplugins\nitePR.prx
 move SRC_nitePR_revJ\nitePR.prx e:\seplugins\nitePR.prx
--- trunk/mk.bat
+++ trunk/mk.bat
@@ -1,9 +1,8 @@
+make
 copy MKIJIRO.prx MKIJIRO\MKIJIRO.prx
-copy MKIJIRO_POPS.prx MKIJIRO\MKIJIRO_POPS.prx
 copy MKIJIRO.prx patches\MKIJIRO.prx
-copy MKIJIRO_POPS.prx patches\MKIJIRO_POPS.prx
 del *.elf
 del objects\*.o
-move MKIJIRO_POPS.prx e:\seplugins\MKIJIRO_POPS.prx
+move MKIJIRO.prx g:\seplugins\MKIJIRO.prx
 move MKIJIRO.prx e:\seplugins\MKIJIRO.prx
 del *.prx
