5d3129d29c414356b89d84ac4907a69d0abc62c9
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2011-02-22 13:46:23 +0000

    半浮動小数表示のバッファがのこってたので修正 半浮動小数の行間がでかい/切り替え時フリーズするのを修正 □押しでパッドコードマスクタイプになるよう変更 CWC<>NPRでDMAの時のみアドレス初期化し、他はフラグをいじるだけに変更

--- NPR.bat
+++ NPR.bat
@@ -3,7 +3,8 @@ make
 cd ..
 del SRC_nitePR_revJ\*.elf
 del SRC_nitePR_revJ\*.o
-copy SRC_nitePR_revJ\nitePR.prx SRC_nitePR_revJ\nitePR\nitePR.prx
-copy SRC_nitePR_revJ\nitePR.prx patches\nitePR.prx
-move SRC_nitePR_revJ\nitePR.prx g:\seplugins\nitePR.prx
-move SRC_nitePR_revJ\nitePR.prx e:\seplugins\nitePR.prx
+copy SRC_nitePR_revJ\nitePRmod.prx SRC_nitePR_revJ\nitePR\nitePRmod.prx
+copy SRC_nitePR_revJ\nitePRmod.prx patches\nitePRmod.prx
+move SRC_nitePR_revJ\nitePRmod.prx g:\seplugins\nitePRmod.prx
+move SRC_nitePR_revJ\nitePRmod.prx e:\seplugins\nitePRmod.prx
+del RC_nitePR_revJ\*.prx
