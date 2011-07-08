25cf158ad5fd915bfc65a038f3e150b3e403194d
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2011-07-08 18:40:23 +0000

    2011/7/09 フォント変更を保存できるようにした GIDとコード内容は等幅フォント以外も選択可能 フォームに文字を入力した際上に保存警告を出すようにした

--- CODEDITOR/CDEMOD/Classes/save database.vb
+++ CODEDITOR/CDEMOD/Classes/save database.vb
@@ -14,6 +14,7 @@ Public Class save_db
         Dim ew As error_window = error_window
         Dim errors As Boolean = False
         Dim cwcar As String = "_L "
+        Dim b1 As String = Nothing
 
         reset_errors() ' Clear prior save errors if any
 
@@ -27,7 +28,6 @@ Public Class save_db
                 For Each n1 As TreeNode In n.Nodes
 
                     If n1.Tag Is Nothing Then
-
                         
                         If n1.Tag.ToString.Substring(0, 1) = "0" Then
                             tw.Write("_C0 " & n1.Text.Trim & vbCrLf)
@@ -135,107 +135,6 @@ Public Class save_db
         tw.Close()
 
     End Sub
-
-    'Public Sub save_pspar(ByVal filename As String, ByVal enc1 As Integer
-    '                      )
-
-    '    Dim m As Main = Main
-    '    Dim i As Integer = 0 ' Error count
-    '    Dim buffer As String()
-
-    '    Dim tw As New StreamWriter(filename, False, _
-    '                               System.Text.Encoding.GetEncoding(enc1))
-    '    Dim ew As error_window = error_window
-    '    Dim errors As Boolean = False
-
-    '    reset_errors() ' Clear prior save errors if any
-
-    '    Try
-
-    '        For Each n As TreeNode In m.codetree.Nodes(0).Nodes
-
-    '            tw.Write("_S " & n.Tag.ToString.Trim & vbCrLf)
-    '            tw.Write("_G " & n.Text.Trim & vbCrLf)
-
-    '            For Each n1 As TreeNode In n.Nodes
-
-    '                If n1.Tag Is Nothing Then
-
-
-    '                    If n1.Tag.ToString.Substring(0, 1) = "0" Then
-    '                        tw.Write("_C0 " & n1.Text.Trim & vbCrLf & "_M 0xCF000000 0x00000000" & vbCrLf)
-    '                    Else
-    '                        tw.Write("_C1 " & n1.Text.Trim & vbCrLf & "_M 0xCF000000 0x00000000" & vbCrLf)
-    '                    End If
-    '                    ' If the code title had no actual codes, don't save it
-    '                    'i += 1
-    '                    'write_errors(i, n.Text.Trim, n1.Text.Trim, "Error:  Code title contained no codes, not saved.")
-    '                    'errors = True
-
-    '                ElseIf n1.Tag.ToString.Trim = "0" Or n1.Tag.ToString.Trim = "1" Then
-
-
-    '                    If n1.Tag.ToString.Substring(0, 1) = "0" Then
-    '                        tw.Write("_C0 " & n1.Text.Trim & vbCrLf & "_M 0xCF000000 0x00000000" & vbCrLf)
-    '                    Else
-    '                        tw.Write("_C1 " & n1.Text.Trim & vbCrLf & "_M 0xCF000000 0x00000000" & vbCrLf)
-    '                    End If
-    '                    ' If the code title had no actual codes, don't save it
-    '                    'i += 1
-    '                    'write_errors(i, n.Text.Trim, n1.Text.Trim, "Error:  Code title contained no codes, not saved.")
-    '                    'errors = True
-
-    '                Else
-
-    '                    If n1.Tag.ToString.Substring(0, 1) = "0" Then
-    '                        tw.Write("_C0 " & n1.Text.Trim & vbCrLf)
-    '                    Else
-    '                        tw.Write("_C1 " & n1.Text.Trim & vbCrLf)
-    '                    End If
-
-
-    '                    buffer = n1.Tag.ToString.Split(CChar(vbCrLf))
-
-    '                    For Each s As String In buffer
-
-    '                        If s.Length > 1 Then
-
-    '                            If s.Contains("#") Then
-
-    '                                tw.Write(s.Trim & vbCrLf)
-
-    '                            Else
-
-    '                                If s.Contains("0x") Then
-
-    '                                    tw.Write("_M " & s.Trim & vbCrLf)
-
-    '                                Else
-    '                                    ' Error, code length was incorrect
-    '                                    i += 1
-    '                                    write_errors(i, n.Text.Trim, n1.Text.Trim, "Incorrectly formatted code: " & s.Trim)
-    '                                    errors = True
-    '                                End If
-
-    '                            End If
-
-    '                        End If
-
-    '                    Next
-
-    '                End If
-
-    '            Next
-
-    '        Next
-
-    '    Catch ex As Exception
-    '        MessageBox.Show(ex.Message)
-    '    End Try
-
-    '    tw.Close()
-
-    'End Sub
 
     Public Sub save_psx(ByVal filename As String, ByVal enc1 As Integer
                         )
