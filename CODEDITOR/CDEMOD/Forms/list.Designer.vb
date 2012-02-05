392bfbedea30372b72d2976fc07a0ec4ac4fab2c
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2012-02-05 13:42:24 +0000

    9660

--- CODEDITOR/CDEMOD/Forms/list.Designer.vb
+++ CODEDITOR/CDEMOD/Forms/list.Designer.vb
@@ -45,30 +45,30 @@ Partial Class list
         '
         'ListView1
         '
-        Me.ListView1.Location = New System.Drawing.Point(16, 15)
+        Me.ListView1.Location = New System.Drawing.Point(14, 14)
         Me.ListView1.Margin = New System.Windows.Forms.Padding(4)
         Me.ListView1.MultiSelect = False
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(287, 279)
+        Me.ListView1.Size = New System.Drawing.Size(252, 261)
         Me.ListView1.TabIndex = 2
         Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'APPLY
         '
-        Me.APPLY.Location = New System.Drawing.Point(16, 302)
+        Me.APPLY.Location = New System.Drawing.Point(14, 282)
         Me.APPLY.Margin = New System.Windows.Forms.Padding(4)
         Me.APPLY.Name = "APPLY"
-        Me.APPLY.Size = New System.Drawing.Size(75, 29)
+        Me.APPLY.Size = New System.Drawing.Size(66, 27)
         Me.APPLY.TabIndex = 5
         Me.APPLY.Text = "適用"
         Me.APPLY.UseVisualStyleBackColor = True
         '
         'lsclose
         '
-        Me.lsclose.Location = New System.Drawing.Point(99, 302)
+        Me.lsclose.Location = New System.Drawing.Point(87, 282)
         Me.lsclose.Margin = New System.Windows.Forms.Padding(4)
         Me.lsclose.Name = "lsclose"
-        Me.lsclose.Size = New System.Drawing.Size(78, 29)
+        Me.lsclose.Size = New System.Drawing.Size(68, 27)
         Me.lsclose.TabIndex = 6
         Me.lsclose.Text = "閉じる"
         Me.lsclose.UseVisualStyleBackColor = True
@@ -85,21 +85,21 @@ Partial Class list
         Me.GroupBox1.Controls.Add(Me.RadioButton3)
         Me.GroupBox1.Controls.Add(Me.RadioButton2)
         Me.GroupBox1.Controls.Add(Me.RadioButton1)
-        Me.GroupBox1.Location = New System.Drawing.Point(312, 8)
+        Me.GroupBox1.Location = New System.Drawing.Point(273, 7)
         Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
-        Me.GroupBox1.Size = New System.Drawing.Size(121, 288)
+        Me.GroupBox1.Size = New System.Drawing.Size(106, 269)
         Me.GroupBox1.TabIndex = 9
         Me.GroupBox1.TabStop = False
         '
         'RadioButton10
         '
         Me.RadioButton10.AutoSize = True
-        Me.RadioButton10.Location = New System.Drawing.Point(9, 266)
+        Me.RadioButton10.Location = New System.Drawing.Point(8, 248)
         Me.RadioButton10.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton10.Name = "RadioButton10"
-        Me.RadioButton10.Size = New System.Drawing.Size(47, 19)
+        Me.RadioButton10.Size = New System.Drawing.Size(37, 17)
         Me.RadioButton10.TabIndex = 9
         Me.RadioButton10.Text = "10:"
         Me.RadioButton10.UseVisualStyleBackColor = True
@@ -107,10 +107,10 @@ Partial Class list
         'RadioButton9
         '
         Me.RadioButton9.AutoSize = True
-        Me.RadioButton9.Location = New System.Drawing.Point(9, 238)
+        Me.RadioButton9.Location = New System.Drawing.Point(8, 222)
         Me.RadioButton9.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton9.Name = "RadioButton9"
-        Me.RadioButton9.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton9.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton9.TabIndex = 8
         Me.RadioButton9.Text = "9:"
         Me.RadioButton9.UseVisualStyleBackColor = True
@@ -118,10 +118,10 @@ Partial Class list
         'RadioButton8
         '
         Me.RadioButton8.AutoSize = True
-        Me.RadioButton8.Location = New System.Drawing.Point(9, 209)
+        Me.RadioButton8.Location = New System.Drawing.Point(8, 195)
         Me.RadioButton8.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton8.Name = "RadioButton8"
-        Me.RadioButton8.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton8.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton8.TabIndex = 7
         Me.RadioButton8.Text = "8:"
         Me.RadioButton8.UseVisualStyleBackColor = True
@@ -129,10 +129,10 @@ Partial Class list
         'RadioButton7
         '
         Me.RadioButton7.AutoSize = True
-        Me.RadioButton7.Location = New System.Drawing.Point(9, 180)
+        Me.RadioButton7.Location = New System.Drawing.Point(8, 168)
         Me.RadioButton7.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton7.Name = "RadioButton7"
-        Me.RadioButton7.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton7.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton7.TabIndex = 6
         Me.RadioButton7.Text = "7:"
         Me.RadioButton7.UseVisualStyleBackColor = True
@@ -140,10 +140,10 @@ Partial Class list
         'RadioButton6
         '
         Me.RadioButton6.AutoSize = True
-        Me.RadioButton6.Location = New System.Drawing.Point(9, 151)
+        Me.RadioButton6.Location = New System.Drawing.Point(8, 141)
         Me.RadioButton6.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton6.Name = "RadioButton6"
-        Me.RadioButton6.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton6.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton6.TabIndex = 5
         Me.RadioButton6.Text = "6:"
         Me.RadioButton6.UseVisualStyleBackColor = True
@@ -151,10 +151,10 @@ Partial Class list
         'RadioButton5
         '
         Me.RadioButton5.AutoSize = True
-        Me.RadioButton5.Location = New System.Drawing.Point(9, 121)
+        Me.RadioButton5.Location = New System.Drawing.Point(8, 113)
         Me.RadioButton5.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton5.Name = "RadioButton5"
-        Me.RadioButton5.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton5.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton5.TabIndex = 4
         Me.RadioButton5.Text = "5:"
         Me.RadioButton5.UseVisualStyleBackColor = True
@@ -162,10 +162,10 @@ Partial Class list
         'RadioButton4
         '
         Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(9, 94)
+        Me.RadioButton4.Location = New System.Drawing.Point(8, 88)
         Me.RadioButton4.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton4.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton4.TabIndex = 3
         Me.RadioButton4.Text = "4:"
         Me.RadioButton4.UseVisualStyleBackColor = True
@@ -173,10 +173,10 @@ Partial Class list
         'RadioButton3
         '
         Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(9, 65)
+        Me.RadioButton3.Location = New System.Drawing.Point(8, 61)
         Me.RadioButton3.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton3.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton3.TabIndex = 2
         Me.RadioButton3.Text = "3:"
         Me.RadioButton3.UseVisualStyleBackColor = True
@@ -184,10 +184,10 @@ Partial Class list
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(9, 36)
+        Me.RadioButton2.Location = New System.Drawing.Point(8, 34)
         Me.RadioButton2.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton2.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton2.TabIndex = 1
         Me.RadioButton2.Text = "2:"
         Me.RadioButton2.UseVisualStyleBackColor = True
@@ -195,10 +195,10 @@ Partial Class list
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(9, 9)
+        Me.RadioButton1.Location = New System.Drawing.Point(8, 8)
         Me.RadioButton1.Margin = New System.Windows.Forms.Padding(4)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(39, 19)
+        Me.RadioButton1.Size = New System.Drawing.Size(31, 17)
         Me.RadioButton1.TabIndex = 0
         Me.RadioButton1.Text = "1:"
         Me.RadioButton1.UseVisualStyleBackColor = True
@@ -206,38 +206,38 @@ Partial Class list
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(312, 308)
+        Me.CheckBox1.Location = New System.Drawing.Point(273, 287)
         Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(67, 19)
+        Me.CheckBox1.Size = New System.Drawing.Size(63, 18)
         Me.CheckBox1.TabIndex = 10
         Me.CheckBox1.Text = "ぐりっど"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'NumericUpDown1
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(392, 305)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(343, 285)
         Me.NumericUpDown1.Margin = New System.Windows.Forms.Padding(4)
         Me.NumericUpDown1.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
         Me.NumericUpDown1.Name = "NumericUpDown1"
-        Me.NumericUpDown1.Size = New System.Drawing.Size(41, 22)
+        Me.NumericUpDown1.Size = New System.Drawing.Size(36, 21)
         Me.NumericUpDown1.TabIndex = 11
         '
         'ls_save
         '
         Me.ls_save.AutoSize = True
-        Me.ls_save.Location = New System.Drawing.Point(184, 308)
+        Me.ls_save.Location = New System.Drawing.Point(161, 287)
         Me.ls_save.Name = "ls_save"
-        Me.ls_save.Size = New System.Drawing.Size(104, 19)
+        Me.ls_save.Size = New System.Drawing.Size(96, 18)
         Me.ls_save.TabIndex = 12
         Me.ls_save.Text = "適用時保存"
         Me.ls_save.UseVisualStyleBackColor = True
         '
         'list
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(443, 336)
+        Me.ClientSize = New System.Drawing.Size(388, 314)
         Me.Controls.Add(Me.ls_save)
         Me.Controls.Add(Me.NumericUpDown1)
         Me.Controls.Add(Me.CheckBox1)
--- CODEDITOR/lba/Form1.Designer.vb
+++ CODEDITOR/lba/Form1.Designer.vb
@@ -22,23 +22,42 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListView2 = New System.Windows.Forms.ListView()
         Me.SuspendLayout()
         '
         'TreeView1
         '
-        Me.TreeView1.Location = New System.Drawing.Point(37, 12)
+        Me.TreeView1.ImageIndex = 0
+        Me.TreeView1.ImageList = Me.ImageList1
+        Me.TreeView1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.TreeView1.LineColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.TreeView1.Location = New System.Drawing.Point(33, 37)
         Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.SelectedImageIndex = 0
         Me.TreeView1.Size = New System.Drawing.Size(293, 357)
         Me.TreeView1.TabIndex = 0
         '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "code title.png")
+        Me.ImageList1.Images.SetKeyName(1, "code selected.png")
+        Me.ImageList1.Images.SetKeyName(2, "file.png")
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(367, 22)
+        Me.Button1.Location = New System.Drawing.Point(33, 8)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(85, 23)
         Me.Button1.TabIndex = 1
@@ -47,35 +66,66 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(336, 97)
+        Me.TextBox1.Location = New System.Drawing.Point(33, 412)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(399, 272)
+        Me.TextBox1.Size = New System.Drawing.Size(399, 107)
         Me.TextBox1.TabIndex = 2
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(467, 55)
+        Me.TextBox2.Location = New System.Drawing.Point(251, 9)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(100, 21)
         Me.TextBox2.TabIndex = 3
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(367, 55)
+        Me.Button2.Location = New System.Drawing.Point(145, 9)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(85, 23)
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "name"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(353, 38)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(535, 356)
+        Me.ListView1.SmallImageList = Me.ImageList1
+        Me.ListView1.Sorting = System.Windows.Forms.SortOrder.Ascending
+        Me.ListView1.TabIndex = 5
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(365, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(22, 14)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "dir"
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(393, 2)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(495, 30)
+        Me.ListView2.TabIndex = 7
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.SmallIcon
+        '
         'Form1
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(770, 381)
+        Me.ClientSize = New System.Drawing.Size(900, 531)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -93,5 +143,9 @@ Partial Class Form1
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
 
 End Class
