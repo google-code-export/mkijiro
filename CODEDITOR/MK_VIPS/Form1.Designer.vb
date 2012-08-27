060b8f3e1da56ff5a1cd9db3822174f9218e69dd
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2012-08-27 14:17:18 +0000

    2012/08/27 角度にHMS,DMS,グラード,∟(直角)単位を追加

--- CODEDITOR/MK_VIPS/Form1.Designer.vb
+++ CODEDITOR/MK_VIPS/Form1.Designer.vb
@@ -40,6 +40,8 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FairuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.open = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ASMopen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CODEopen = New System.Windows.Forms.ToolStripMenuItem()
         Me.save = New System.Windows.Forms.ToolStripMenuItem()
         Me.ASM保存ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.コード保存ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -50,8 +52,6 @@ Partial Class Form1
         Me.設定ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.バージョン = New System.Windows.Forms.ToolStripMenuItem()
         Me.savea = New System.Windows.Forms.Button()
-        Me.ASMopen = New System.Windows.Forms.ToolStripMenuItem()
-        Me.CODEopen = New System.Windows.Forms.ToolStripMenuItem()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
@@ -225,14 +225,26 @@ Partial Class Form1
         '
         Me.open.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ASMopen, Me.CODEopen})
         Me.open.Name = "open"
-        Me.open.Size = New System.Drawing.Size(152, 22)
+        Me.open.Size = New System.Drawing.Size(100, 22)
         Me.open.Text = "開く"
+        '
+        'ASMopen
+        '
+        Me.ASMopen.Name = "ASMopen"
+        Me.ASMopen.Size = New System.Drawing.Size(109, 22)
+        Me.ASMopen.Text = "ASM"
+        '
+        'CODEopen
+        '
+        Me.CODEopen.Name = "CODEopen"
+        Me.CODEopen.Size = New System.Drawing.Size(109, 22)
+        Me.CODEopen.Text = "CODE"
         '
         'save
         '
         Me.save.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ASM保存ToolStripMenuItem, Me.コード保存ToolStripMenuItem})
         Me.save.Name = "save"
-        Me.save.Size = New System.Drawing.Size(152, 22)
+        Me.save.Size = New System.Drawing.Size(100, 22)
         Me.save.Text = "保存"
         '
         'ASM保存ToolStripMenuItem
@@ -250,12 +262,12 @@ Partial Class Form1
         'ToolStripSeparator1
         '
         Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
-        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(97, 6)
         '
         '終了
         '
         Me.終了.Name = "終了"
-        Me.終了.Size = New System.Drawing.Size(152, 22)
+        Me.終了.Size = New System.Drawing.Size(100, 22)
         Me.終了.Text = "終了"
         '
         'enc
@@ -290,18 +302,6 @@ Partial Class Form1
         Me.savea.TabIndex = 7
         Me.savea.Text = "ASM保存"
         Me.savea.UseVisualStyleBackColor = True
-        '
-        'ASMopen
-        '
-        Me.ASMopen.Name = "ASMopen"
-        Me.ASMopen.Size = New System.Drawing.Size(152, 22)
-        Me.ASMopen.Text = "ASM"
-        '
-        'CODEopen
-        '
-        Me.CODEopen.Name = "CODEopen"
-        Me.CODEopen.Size = New System.Drawing.Size(152, 22)
-        Me.CODEopen.Text = "CODE"
         '
         'Form1
         '
--- CODEDITOR/MK_VIPS/ver.Designer.vb
+++ CODEDITOR/MK_VIPS/ver.Designer.vb
@@ -67,9 +67,9 @@ Partial Class version
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(18, 50)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(142, 12)
+        Me.Label3.Size = New System.Drawing.Size(130, 12)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "BUILD:2012/08/24-2 22:22"
+        Me.Label3.Text = "BUILD:2012/08/27 23:25"
         '
         'Label1
         '
