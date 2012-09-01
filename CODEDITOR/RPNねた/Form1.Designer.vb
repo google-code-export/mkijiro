6cf4b6b6cc10cac6e7847dfb01402edbe787fa9a
imasperfume <imasperfume@15ff8059-0882-7155-0bc6-e0013635077f>
2012-09-01 14:22:12 +0000

    //四則演算 "+","-","/" //2引数が必要な関数(pow(x,y),logx(x,y)=lnY/lnX) "logx", "pow", "xrt", "yrt", "logy", "atan2_r", "atan2_g", "atan2_d", "atan2_", //角度変換,deg=度,rad=ラジアン,grad=グラード,r=∟ deg2rad", "deg2grad", "deg2r", rad2deg", "rad2grad", "rad2r", "grad2deg", "grad2rad", "grad2r", "r2deg", "r2rad", "r2grad", //三角関数 "tanhr", "coshr", "sinhr", _ "tanhg", "coshg", "sinhg", _ "tanhd", "coshd", "sinhd", _ "tanh", "cosh", "sinh", _ "atanr", "acosr", "asinr", "tanr", "cosr", "sinr", _ "atang", "acosg", "asing", "tang", "cosg", "sing", _ "atand", "acosd", "asind", "tand", "cosd", "sind", _ "atan", "acos", "asin", "tan", "cos", "sin", _ //他 "sqrt", "cbrt", "log", "ln", "reci", "√"}
    2012/09/01
    とりあえず作成

---
+++ CODEDITOR/RPNねた/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LOOKSORDER = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(237, 43)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "4*atan(5^(-1))-atan(pow(239,-1))"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "数式"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(3, 88)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(237, 48)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "RPN式"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(5, 217)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(133, 19)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 202)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "計算結果"
+        '
+        'LOOKSORDER
+        '
+        Me.LOOKSORDER.AutoSize = True
+        Me.LOOKSORDER.Checked = True
+        Me.LOOKSORDER.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.LOOKSORDER.Location = New System.Drawing.Point(10, 143)
+        Me.LOOKSORDER.Name = "LOOKSORDER"
+        Me.LOOKSORDER.Size = New System.Drawing.Size(101, 40)
+        Me.LOOKSORDER.TabIndex = 7
+        Me.LOOKSORDER.Text = "関数処理順番;" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "□Y,X スタック順" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "☑X,Y 見た目順"
+        Me.LOOKSORDER.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "数式→RPN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(153, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "計算"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.LOOKSORDER)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 5)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(255, 245)
+        Me.Panel1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(273, 262)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "RPNねた"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents LOOKSORDER As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+
+End Class
---
+++ CODEDITOR/RPNねた/Form1.vb
@@ -0,0 +1,247 @@
+﻿Imports System
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.IO
+
+Public Class Form1
+
+    Dim mathsconst As String() = {"π", "円周率", "黄金比", "自然対数の底"}
+    Dim mathrp As String() = {"pi", "pi", "goldenratio", "e"}
+
+    Private Function cvt_dbl(ByVal s As String) As Double
+        Dim dem As Double = 0
+        Dim cnst As New Regex("-?(e|pi|goldenratio)")
+        Dim cnstm As Match = cnst.Match(s)
+        Dim frac As New Regex("-?\d+\.?\d*")
+        Dim fracm As Match = frac.Match(s)
+        If cnstm.Success Then
+            If cnstm.Value.Contains("e") Then
+                dem = (Math.E)
+            ElseIf cnstm.Value.Contains("pi") Then
+                dem = (Math.PI)
+            ElseIf cnstm.Value.Contains("goldendratio") Then
+                dem = ((1 + Math.Sqrt(5)) / 2)
+            End If
+            If cnstm.Value.Contains("-") Then
+                dem = -dem
+            End If
+        ElseIf fracm.Success Then
+            dem = Convert.ToDouble(fracm.Value)
+        End If
+
+        Return dem
+    End Function
+
+    Private Function swapper(ByVal dem As Double()) As Double()
+        Dim demt As Double
+        demt = dem(1)
+        dem(1) = dem(0)
+        dem(0) = demt
+        Return dem
+    End Function
+
+    Private Function swapper2(ByVal dem As Double()) As Double()
+        If LOOKSORDER.Checked Then
+            dem = swapper(dem)
+        End If
+        Return dem
+    End Function
+
+    Private Function rpndbl(ByVal str As String) As Double
+        Dim ss As String() = str.ToLower.Split(CChar(","))
+        Dim len As Integer = ss.Length - 1
+        Dim dem(len) As Double
+        For i = 0 To len
+            Select Case ss(i).Trim
+                '4*(4*atan(1/5)-atan(1/239))
+                '4,4,5,1/x,atan,*,239,1/x,atan,-,*
+                Case "drop"
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "swap"
+                    dem = swapper(dem)
+                Case "deg2rad"
+                    dem(0) = dem(0) * Math.PI / 180
+                Case "deg2grad"
+                    dem(0) = dem(0) * 100 / 90
+                Case "deg2r"
+                    dem(0) = dem(0) / 90
+                Case "rad2deg"
+                    dem(0) = dem(0) * 180 / Math.PI
+                Case "rad2grad"
+                    dem(0) = dem(0) * 200 / Math.PI
+                Case "rad2r"
+                    dem(0) = dem(0) * 2 / Math.PI
+                Case "grad2deg"
+                    dem(0) = dem(0) * 90 / 100
+                Case "grad2rad"
+                    dem(0) = dem(0) * Math.PI / 200
+                Case "grad2r"
+                    dem(0) = dem(0) / 100
+                Case "r2deg"
+                    dem(0) = dem(0) * 90
+                Case "r2rad"
+                    dem(0) = dem(0) * Math.PI / 2
+                Case "r2grad"
+                    dem(0) = dem(0) * 100
+                Case "="
+                Case "+"
+                    dem(1) = dem(0) + dem(1)
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "-"
+                    dem(1) = dem(1) - dem(0)
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "*"
+                    dem(1) = dem(1) * dem(0)
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "/"
+                    dem(1) = dem(1) / dem(0)
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "rpow", "y^x"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Pow(dem(1), dem(0))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "pow", "x^y", "^"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Pow(dem(0), dem(1))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "xrt"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Pow(dem(1), 1 / dem(0))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "yrt"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Pow(dem(0), 1 / dem(1))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "logy"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Log(dem(0), dem(1))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "logx"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Log(dem(1), dem(0))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "1/x", "reci"
+                    dem(0) = 1 / dem(0)
+                Case "sqrt", "√"
+                    dem(0) = Math.Sqrt(dem(0))
+                Case "cbrt"
+                    dem(0) = Math.Pow(dem(0), 1 / 3)
+                Case "log"
+                    dem(0) = Math.Log(dem(0), 10)
+                Case "ln"
+                    dem(0) = Math.Log(dem(0), Math.E)
+                Case "atand"
+                    dem(0) = Math.Atan(dem(0)) * 180 / Math.PI
+                Case "atan"
+                    dem(0) = Math.Atan(dem(0))
+                Case "atanr"
+                    dem(0) = Math.Atan(dem(0)) * 2 / Math.PI
+                Case "atang"
+                    dem(0) = Math.Atan(dem(0)) * 200 / Math.PI
+                Case "atan2_d"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Atan2(dem(0), dem(1)) * 180 / Math.PI
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "atan2_"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Atan2(dem(0), dem(1))
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "atan2_r"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Atan2(dem(0), dem(1)) * 2 / Math.PI
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "atan2_g"
+                    dem = swapper2(dem)
+                    dem(1) = Math.Atan2(dem(0), dem(1)) * 200 / Math.PI
+                    Array.Copy(dem, 1, dem, 0, len)
+                Case "acosd"
+                    dem(0) = Math.Acos(dem(0)) * 180 / Math.PI
+                Case "acos"
+                    dem(0) = Math.Acos(dem(0))
+                Case "acosr"
+                    dem(0) = Math.Acos(dem(0)) * 2 / Math.PI
+                Case "acosg"
+                    dem(0) = Math.Acos(dem(0))
+                Case "asind"
+                    dem(0) = Math.Asin(dem(0)) * 180 / Math.PI
+                Case "asin"
+                    dem(0) = Math.Asin(dem(0))
+                Case "asinr"
+                    dem(0) = Math.Asin(dem(0)) * 2 / Math.PI
+                Case "asing"
+                    dem(0) = Math.Asin(dem(0))
+                Case "tanhd"
+                    dem(0) = Math.Tanh(dem(0) * 200 / Math.PI)
+                Case "tahn"
+                    dem(0) = Math.Tanh(dem(0))
+                Case "tanhr"
+                    dem(0) = Math.Tanh(dem(0) * Math.PI / 2)
+                Case "tanhg"
+                    dem(0) = Math.Tanh(dem(0) * 90 / 100 * Math.PI / 180)
+                Case "coshd"
+                    dem(0) = Math.Cosh(dem(0) * Math.PI / 180)
+                Case "cohs"
+                    dem(0) = Math.Cosh(dem(0))
+                Case "coshr"
+                    dem(0) = Math.Cosh(dem(0) * Math.PI / 2)
+                Case "coshg"
+                    dem(0) = Math.Cosh(dem(0) * 90 / 100 * Math.PI / 180)
+                Case "sinhd"
+                    dem(0) = Math.Sinh(dem(0) * Math.PI / 180)
+                Case "sinh"
+                    dem(0) = Math.Sinh(dem(0))
+                Case "sinhr"
+                    dem(0) = Math.Sinh(dem(0) * Math.PI / 2)
+                Case "sinhg"
+                    dem(0) = Math.Sinh(dem(0) * Math.PI / 200)
+                Case "tand"
+                    dem(0) = Math.Tan(dem(0) * Math.PI / 180)
+                Case "tan"
+                    dem(0) = Math.Tan(dem(0))
+                Case "tanr"
+                    dem(0) = Math.Tan(dem(0) * Math.PI / 2)
+                Case "tang"
+                    dem(0) = Math.Tan(dem(0) * Math.PI / 200)
+                Case "cosd"
+                    dem(0) = Math.Cos(dem(0) * Math.PI / 180)
+                Case "cos"
+                    dem(0) = Math.Cos(dem(0))
+                Case "cosr"
+                    dem(0) = Math.Cos(dem(0) * Math.PI / 2)
+                Case "cosg"
+                    dem(0) = Math.Cos(dem(0) * 90 / 100 * Math.PI / 180)
+                Case "sind"
+                    dem(0) = Math.Sin(dem(0) * Math.PI / 180)
+                Case "sin"
+                    dem(0) = Math.Sin(dem(0))
+                Case "sinr"
+                    dem(0) = Math.Sin(dem(0) * Math.PI / 2)
+                Case "sing"
+                    dem(0) = Math.Sin(dem(0) * 90 / 100 * Math.PI / 180)
+                Case Else
+                    If (ss(i)).Trim <> "" Then
+                        Array.Copy(dem, 0, dem, 1, len)
+                        dem(0) = cvt_dbl(ss(i))
+                    End If
+            End Select
+        Next
+        dem(0) = Math.Round(dem(0), 14)
+
+        Return dem(0)
+
+    End Function
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim d As Double = rpndbl(TextBox2.Text)
+        TextBox3.Text = d.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim s As String = TextBox1.Text.Trim
+        Dim p As New Polish
+        TextBox2.Text = p.Main(s, LOOKSORDER.Checked)
+
+    End Sub
+End Class
+
+
